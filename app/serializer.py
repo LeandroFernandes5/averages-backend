1119a337b657a7fcf701fc8e40887f81586685f7
Leandro Fernandes <fernandesleandro1996@gmail.com>
2021-06-21 23:54:17 +0100

    Remove marshmallow import

--- app/serializer.py
+++ app/serializer.py
@@ -1,11 +1,11 @@
-from app import db, ma
-from app.models import User 
+# # from app import db, ma
+# from app.models import User 
 
 
-class UserSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-       fields = ("id", "email", "name", "role")
-       load_only = ("password",)
+# class UserSchema(ma.SQLAlchemyAutoSchema):
+#     class Meta:
+#        fields = ("id", "email", "name", "role")
+#        load_only = ("password",)
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+# user_schema = UserSchema()
+# users_schema = UserSchema(many=True)
