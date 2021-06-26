a945b6e1381c6f9e076c1e18b9089287ad19d592
Leandro Fernandes <fernandesleandro1996@gmail.com>
2021-06-26 11:51:58 +0100

    Test for CORS implementation

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow 
-# from flask_cors import CORS
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 # 
 #   Get environment configuration 
 #  
