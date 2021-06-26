87fb71bf8ae8b98c525ed34f9ead0b4ab1913927
Leandro Fernandes <fernandesleandro1996@gmail.com>
2021-06-26 10:36:05 +0100

    Added CORS package and Code

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow 
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-
+CORS(app)
 # 
 #   Get environment configuration 
 #  
