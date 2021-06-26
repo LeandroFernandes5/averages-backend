716e2c41021482f70ba8cc63e983fdb1867da554
Leandro Fernandes <fernandesleandro1996@gmail.com>
2021-06-26 11:15:22 +0100

    Flask migrate

--- app/models.py
+++ app/models.py
@@ -4,25 +4,23 @@ from app import db, ma
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.dialects.postgresql import ENUM as pgEnum
 from sqlalchemy.sql import func
-# from dataclasses import dataclass
 import datetime
 
 
-# @dataclass
 class User(db.Model):
  
     __tablename__ = 'users'
 
-    id = db.Column('user_id', db.Integer, primary_key=True)
+    id = db.Column('id', db.Integer, primary_key=True)
     email = db.Column('email', db.String(128), index=True, unique=True, nullable=False)
     password = db.Column('password', db.String(128), nullable=False, unique=False)
     name = db.Column('name', db.String(64), nullable=False, unique=False)
     role = db.Column('role', db.String(64), nullable=False, unique=False, server_default='Driver')
-    image = db.Column('image', db.String(128), nullable=True, unique=False)
-    password_reset_token = db.Column('password_reset_token', db.String(128), nullable=True,  unique=False)
+    imageURL = db.Column('image', db.String(128), nullable=True, unique=False)
+    passwordResetToken = db.Column('password_reset_token', db.String(128), nullable=True,  unique=False)
     status = db.Column('status', db.String(64), nullable=False, unique=False, server_default='Pending')
-    created_date = db.Column('created_date', db.DateTime, nullable=False,  unique=False, server_default=func.now())
-    modified_date = db.Column('modified_date', db.DateTime, nullable=True,  unique=False, onupdate=func.now())
+    createdDate = db.Column('createdDate', db.DateTime, nullable=False,  unique=False, server_default=func.now())
+    modifiedDate = db.Column('modifiedDate', db.DateTime, nullable=True,  unique=False, onupdate=func.now())
     driver = relationship("Driver", uselist=False, backref="user")
 
     #
@@ -38,12 +36,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.email)    
 
-# @dataclass
+
 class Driver(db.Model):
 
     __tablename__ = 'drivers'
 
-    id = db.Column('driver_id', db.Integer, primary_key=True)
+    id = db.Column('id', db.Integer, primary_key=True)
     image = db.Column('image', db.String(128), nullable=True, unique=False)
     name = db.Column('name', db.String(64), nullable=False, unique=False)
     birthDate = db.Column('birth_date', db.DateTime, nullable=True, unique=False)
@@ -53,9 +51,9 @@ class Driver(db.Model):
     driverLicenseExpireDate = db.Column('driver_license_expire_date', db.DateTime, nullable=True, unique=False)
     tccExpireDate = db.Column('tcc_expire_date', db.DateTime, nullable=True, unique=False)
     camExpireDate = db.Column('cam_expire_date', db.DateTime, nullable=True, unique=False)
-    created_date = db.Column('created_date', db.DateTime, nullable=False,  unique=False, server_default=func.now())
-    modified_date = db.Column('modified_date', db.DateTime, nullable=True,  unique=False, onupdate=func.now())
-    user_id = db.Column('user_id', db.Integer, db.ForeignKey('users.user_id'))
+    createdDate = db.Column('createdDate', db.DateTime, nullable=False,  unique=False, server_default=func.now())
+    modifiedDate = db.Column('modifiedDate', db.DateTime, nullable=True,  unique=False, onupdate=func.now())
+    userId = db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
     # supplies = db.relationship('Post', backref='driver', lazy='dynamic')
 
     def __repr__(self):
@@ -64,7 +62,7 @@ class Driver(db.Model):
 # Product Schema
 class DriverSchema(ma.Schema):
   class Meta:
-    fields = ('id', 'name', 'birthDate', 'ccNumber', 'ccExpireDate', 'driverLicenseNumber', 'driverLicenseExpireDate', 'tccExpireDate', 'camExpireDate', 'user_id')
+    fields = ('id', 'name', 'birthDate', 'ccNumber', 'ccExpireDate', 'driverLicenseNumber', 'driverLicenseExpireDate', 'tccExpireDate', 'camExpireDate', 'userid')
 
 # Init schema
 driver_schema = DriverSchema()
