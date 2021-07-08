d2aa7d14948bf7d963cae0f79911c33fc9d2cab9
Leandro Fernandes <fernandesleandro1996@gmail.com>
2021-07-08 18:57:54 +0100

    Added Supplies Test Data

--- seeders/seeder.py
+++ seeders/seeder.py
@@ -1,8 +1,9 @@
 import os
+from random import *
 import csv
-from sqlalchemy import Column, Integer, String,  create_engine, insert, MetaData, Table, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String,  create_engine, insert, MetaData, Table, DateTime, Float, Boolean
 from werkzeug.security import generate_password_hash
+import datetime
 
 
 dbURI = os.environ.get('DATABASE_URL') or 'postgresql+psycopg2://averages:averages@localhost:6543/flask'     
@@ -13,7 +14,7 @@ conn = engine.connect()
 meta = MetaData()
 
 tables = engine.table_names()
-
+print(tables)
 for table in tables:
     if table == 'alembic_version':
         pass
@@ -91,27 +92,97 @@ with open('drivers.csv') as csvfile:
         try:
              conn.engine.execute(insert(drivers).
                 values(
-                    name= row[0],
-                    birthDate=row[1],
-                    ccNumber=row[2],
-                    ccExpireDate=row[3],
-                    driverLicenseNumber=row[4],
-                    driverLicenseExpireDate=row[5],
-                    tccExpireDate=row[6],
-                    camExpireDate=row[7],
-                    userId = row[8]
+                    name = row[0],
+                    birthDate = datetime.datetime.now() - datetime.timedelta(weeks=randint(2000, 3000)),
+                    ccNumber = row[1],
+                    ccExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(40, 80)),
+                    driverLicenseNumber = row[2],
+                    driverLicenseExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(10, 40)),
+                    tccExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(5, 12)),
+                    camExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(15, 23)),
+                    userId = row[3]
                     ))
         except IndexError:
             conn.engine.execute(insert(drivers).
                 values(
-                    name= row[0],
-                    birthDate=row[1],
-                    ccNumber=row[2],
-                    ccExpireDate=row[3],
-                    driverLicenseNumber=row[4],
-                    driverLicenseExpireDate=row[5],
-                    tccExpireDate=row[6],
-                    camExpireDate=row[7]
+                    name = row[0],
+                    birthDate = datetime.datetime.now() - datetime.timedelta(weeks=randint(2000, 3000)),
+                    ccNumber = row[1],
+                    ccExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(40, 80)),
+                    driverLicenseNumber = row[2],
+                    driverLicenseExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(10, 40)),
+                    tccExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(5, 12)),
+                    camExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(15, 23))
                     )
 
         )
+
+##
+#   Insert Cars
+##
+
+cars = Table('cars', 
+meta,
+Column('plate', String(64), nullable=False, unique=False),
+Column('brand', String(64), nullable=True, unique=False),
+Column('model', String(64), nullable=True, unique=False),
+Column('registerDate', DateTime(timezone=True), nullable=True, unique=False),
+Column('chassisNo', String(64), nullable=True, unique=False),
+Column('obraNo', String(64), nullable=True, unique=False),
+Column('inspectionDate', DateTime(timezone=True), nullable=True, unique=False),
+Column('tccExpireDate', DateTime(timezone=True), nullable=True, unique=False),
+Column('licenseExpireDate', DateTime(timezone=True), nullable=True, unique=False),
+Column('tachographDate', DateTime(timezone=True), nullable=True, unique=False),
+Column('status', String(64), nullable=False, unique=False, server_default='Active')
+)
+
+with open('cars.csv') as csvfile:
+     userader = csv.reader(csvfile, delimiter=',')
+     for row in userader:
+            conn.engine.execute(insert(cars).
+            values(
+                plate= row[0],
+                brand=row[1],
+                model=row[2],
+                registerDate=datetime.datetime.now() - datetime.timedelta(weeks=randint(400, 1000)),
+                chassisNo=row[3],
+                obraNo=row[4],
+                inspectionDate= datetime.datetime.now() + datetime.timedelta(weeks=randint(10, 40)),
+                tccExpireDate= datetime.datetime.now() + datetime.timedelta(weeks=randint(400, 1000)),
+                licenseExpireDate = datetime.datetime.now() + datetime.timedelta(weeks=randint(20, 50)),
+                tachographDate=datetime.datetime.now() + datetime.timedelta(weeks=randint(400, 1000)),
+                status=row[5]
+                ))
+
+
+##
+#   Insert Supplies
+##
+
+supplies = Table('supplies', 
+meta,
+Column('totalKm', Integer, nullable=False, unique=False),
+Column('liters', Float(precision=10, decimal_return_scale=2), nullable=False, unique=False),
+Column('fullTank', Boolean, nullable=False, unique=False),
+Column('cost', Float(precision=10, decimal_return_scale=2), nullable=False, unique=False, default=0),
+Column('supplyDate', DateTime(timezone=True), nullable=False,  unique=False),
+Column('average', Float(precision=10, decimal_return_scale=2), nullable=False, unique=False),
+Column('driverId', Integer),
+Column('gasStationId',Integer),
+Column('carId',Integer),
+)
+
+with open('supplies.csv') as csvfile:
+     userader = csv.reader(csvfile, delimiter=',')
+     for row in userader:
+            conn.engine.execute(insert(supplies).
+            values(
+                totalKm = randint(50,10000),
+                liters = randint(10,30),
+                fullTank = random() < 0.5,
+                supplyDate = datetime.datetime.now() - datetime.timedelta(weeks=randint(1, 3)),
+                average = row[0],
+                driverId = randint(1, 2),
+                gasStationId = randint(1, 3),
+                carId = randint(1, 3)
+                ))
